66a126e80514994dcf3335792f4ff8975d9fa4bc
Malcolm Neal <malcolm259@gmail.com>
2022-01-27 19:29:46 -0800

    Added basic farmer AI

--- GGJProject/Assets/_GAME/Scripts/Farmer.cs
+++ GGJProject/Assets/_GAME/Scripts/Farmer.cs
@@ -8,17 +8,52 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class Farmer : NetworkBehaviour
 {
-    private NavMeshAgent navAgent;
+    private const int ANIM_IDLE = 0;
+    private const int ANIM_RUN = 1;
     
+    public Animator animator;
+    public float nextRandomPositionDistThreshold = 0.3f;
+    public float nextRandomPositionRange = 2.0f;
+    
+    private NavMeshAgent navAgent;
+    private bool isMovingRandomly;
+    private float moveRandomTimer;
+
     private void OnEnable()
     {
         navAgent = this.GetComponent<NavMeshAgent>();
     }
 
-    [Server]
-    private void BeginMoveRandomly()
+    [ContextMenu("Start Moving Randomly")]
+    public void MoveRandomly()
     {
-        
+        Debug.Log("Start Moving Randomly");
+        isMovingRandomly = true;
+    }
+
+    public void StopMovingRandomly()
+    {
+        Debug.Log("Stop Moving Randomly");
+        isMovingRandomly = false;
+    }
+    
+    public void MoveTo(Vector3 position)
+    {
+        Debug.Log("Attempting to go to position: " + position);
+        if (navAgent != null)
+        {
+            navAgent.SetDestination(position);
+            SetAnimation(ANIM_RUN);
+        }
+    }
+
+    public void Stop()
+    {
+        if (navAgent != null)
+        {
+            navAgent.isStopped = true;
+            SetAnimation(ANIM_IDLE);
+        }
     }
 
     [Command]
@@ -33,11 +68,29 @@ public class Farmer : NetworkBehaviour
         // set shader hit amount
     }
 
-    public void MoveTo(Vector3 position)
+    private void Update()
     {
-        if (navAgent != null)
+        if (isMovingRandomly)
         {
-            navAgent.SetDestination(position);
+            float distToDestination = Vector3.Distance(transform.position, navAgent.destination);
+            if (distToDestination < nextRandomPositionDistThreshold)
+            {
+                // choose another random destination
+                Vector3 targetDest = GameManager.Instance.gameCameraDollyTrack.FollowTargetPosition;
+                targetDest.x += UnityEngine.Random.Range(-nextRandomPositionRange, nextRandomPositionRange);
+                targetDest.z += UnityEngine.Random.Range(-nextRandomPositionRange, nextRandomPositionRange);
+                
+                MoveTo(targetDest);
+            }
+            moveRandomTimer += Time.deltaTime;
+        }
+    }
+
+    private void SetAnimation(int val)
+    {
+        if (animator != null)
+        {
+            animator.SetInteger("Animation", val);
         }
     }
 }
