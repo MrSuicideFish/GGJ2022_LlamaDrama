e85fff26027e31d980c1249b110b261722bb16cd
LAPTOP-AL6KVBSE\MacZ <Malcolm259@gmail.com>
2022-03-09 10:28:13 -0500

    updated main menu

--- GGJProject/Assets/_GAME/Scripts/SettingsManager.cs
+++ GGJProject/Assets/_GAME/Scripts/SettingsManager.cs
@@ -2,17 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SettingsManager : MonoBehaviour
+public static class SettingsManager
 {
-    // Start is called before the first frame update
-    void Start()
+    private static SettingScreen screenInst;
+
+    private static float GameVolume
     {
-        
+        get
+        {
+            return PlayerPrefs.GetFloat("GameVolume", 1.0f);
+        }
+
+        set
+        {
+            PlayerPrefs.SetFloat("GameVolume", value);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public static void OpenSettings()
     {
+        if (screenInst == null)
+        {
+            SettingScreen scrnRes = Resources.Load<SettingScreen>("UI/SettingScreen");
+            if (scrnRes != null)
+            {
+                screenInst = SettingScreen.Instantiate(scrnRes);
+            }
+        }
         
+        screenInst.Show();
+    }
+
+    public static void CloseSettings()
+    {
+        if (screenInst == null)
+        {
+            SettingScreen scrnRes = Resources.Load<SettingScreen>("UI/SettingScreen");
+            if (scrnRes != null)
+            {
+                screenInst = SettingScreen.Instantiate(scrnRes);
+            }
+        }
+        
+        screenInst.Hide();
+    }
+
+    public static void SetVolume(float vol)
+    {
+        GameVolume = vol;
+    }
+
+    public static float GetVolume()
+    {
+        return GameVolume;
     }
 }
--- GGJProject/Assets/_GAME/Scripts/UI/SettingScreen.cs
+++ GGJProject/Assets/_GAME/Scripts/UI/SettingScreen.cs
@@ -1,8 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingScreen : MonoBehaviour
 {
-    
+    public void Show()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        this.gameObject.SetActive(false);
+
+    }
 }
